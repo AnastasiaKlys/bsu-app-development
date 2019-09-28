a2e331d274a663b571fd5a48dd6642aae018a193
Anastasiya Klys <anastasya.klys@gmail.com>
2019-09-28 11:44:53 +0300

    first tast

---
+++ firstTask/app/src/main/java/com/companyname/myapplication/InputSomeTextActivity.java
@@ -0,0 +1,30 @@
+package com.companyname.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class InputSomeTextActivity extends AppCompatActivity {
+    Button btn_input_ok;
+    EditText input_mood;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_some_text);
+        input_mood = findViewById(R.id.input_mood);
+        btn_input_ok = findViewById(R.id.btn_input_ok);
+        btn_input_ok.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("mood", input_mood.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ firstTask/app/src/main/java/com/companyname/myapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.companyname.myapplication;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int PICK_MOOD_REQUEST = 1;
+    Button button;
+    TextView textViewMood;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewMood = findViewById(R.id.textViewMood);
+        button = findViewById(R.id.btn_ok);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), InputSomeTextActivity.class);
+                startActivityForResult(intent, PICK_MOOD_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_MOOD_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String mood = data.getStringExtra("mood");
+                textViewMood.setText(mood);
+            }
+        }
+
+    }
+}
